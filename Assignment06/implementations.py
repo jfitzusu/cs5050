c0c7ee579be181767ddf8152941bce68f5111e94
jfitzusu <A02261889@usu.edu>
2022-11-09 16:37:25 -0700

    Finished Problem 1

--- Assignment06/implementations.py
+++ Assignment06/implementations.py
@@ -1,6 +1,175 @@
+import copy
 import random
 
 import numpy as np
+
+"""
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+PROBLEM 1
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+"""
+
+def generateConcat(n, numberWords=10, minWord=2, maxWord=8):
+    words = []
+    for i in range(numberWords):
+        size = random.randint(minWord, maxWord)
+        word = []
+        for j in range(size):
+            word.append(chr(random.randint(ord('a'), ord('z'))))
+        words.append(''.join(word))
+
+    string = []
+    stringSize = 0
+    while stringSize < n:
+        randomWord = words[random.randint(0, numberWords - 1)]
+        stringSize += len(randomWord)
+        string.append(randomWord)
+
+
+    isNotConcat = bool(random.randint(0, 1))
+
+    if isNotConcat:
+        string.append('Z')
+
+    return ''.join(string), words
+
+
+def concatRecursive(S, words):
+    dictionary = {}
+    for word in words:
+        dictionary[word] = True
+    return concatRecursiveHelper(S, 0, dictionary)
+
+
+def concatRecursiveHelper(S, index, dictionary):
+    if index >= len(S):
+        return True
+
+    for i in range(index + 1, len(S) + 1):
+        if S[index: i] in dictionary and concatRecursiveHelper(S, i, dictionary):
+            return True
+
+    return False
+
+
+def concatMemoizing(S, words):
+    dictionary = {}
+    cache = [None for i in range(len(S))]
+    for word in words:
+        dictionary[word] = True
+    return concatMemoizingHelper(S, 0, dictionary, cache)
+
+
+def concatMemoizingHelper(S, index, dictionary, cache):
+    if index >= len(S):
+        return True
+
+    if cache[index] is not None:
+        return cache[index]
+
+    for i in range(index + 1, len(S) + 1):
+        if S[index: i] in dictionary and concatMemoizingHelper(S, i, dictionary, cache):
+            cache[index] = True
+            return True
+
+    cache[index] = False
+    return False
+
+
+def concatDP(S, words, traceback=False):
+    dictionary = {}
+    for word in words:
+        dictionary[word] = True
+    return concatDPHelper(S, dictionary, traceback)
+
+
+def concatDPHelper(S, dictionary, traceback):
+    results = [False for i in range(len(S) + 1)]
+    results[0] = True
+    for i in range(1, len(S) + 1):
+        for j in range(i):
+            if results[j] and S[j:i] in dictionary:
+                results[i] = True
+
+    if traceback:
+        return results
+
+    return results[-1]
+
+
+
+def concatTraceback(results, S, words):
+    dictionary = {}
+    for word in words:
+        dictionary[word] = True
+    return concatTracebackHelper(results, S, dictionary)
+
+def concatTracebackHelper(results, S, dictionary):
+    i = len(results) - 1
+    words = []
+    while i > 0:
+        for j in range(i - 1, -1, -1):
+            if S[j: i] in dictionary and results[j]:
+                words.append(S[j:i])
+                i = j
+                break
+    words.reverse()
+    return words
+
+
+
+def printConcat(words, S, wordList):
+    print("WORDS:")
+    print(wordList)
+    print("ORIGINAL STRING:")
+    print(S)
+    for i, word in enumerate(words):
+        if i == len(words) - 1:
+            print(f"{word}", end='')
+        else:
+            print(f"{word} | ", end='')
+
+
+"""
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+PROBLEM 2
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+"""
+"""
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+PROBLEM 3
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+"""
+"""
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+PROBLEM 4
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+"""
+"""
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+PROBLEM 5
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+"""
 
 
 def generateNim(n):
@@ -45,7 +214,7 @@ def nimMemoizingHelper(V, startIndex, size, total, cache):
     return result
 
 
-def nimDP(V):
+def nimDP(V, traceBack=False):
     resultsTable = np.array(np.zeros((len(V), len(V))))
     for i in range(len(resultsTable)):
         resultsTable[i][0] = V[i]
@@ -56,4 +225,38 @@ def nimDP(V):
             total += V[i + j]
             resultsTable[i][j] = total - min(resultsTable[i + 1][j - 1], resultsTable[i][j - 1])
 
+    if traceBack:
+        return resultsTable
+
     return resultsTable[0][-1]
+
+
+def nimTraceback(results):
+    moves = []
+    i = 0
+    j = len(results) - 1
+
+    while j > 0:
+        if results[i][j - 1] < results[i + 1][j - 1]:
+            moves.append(-1)
+        else:
+            moves.append(0)
+            i += 1
+        j -= 1
+    moves.append(0)
+
+    return moves
+
+
+def printNim(moves, V):
+    board = copy.deepcopy(V)
+    print("GAME START:")
+    print(board)
+    playerTurn = True
+
+    for move in moves:
+        print(f"Player {1 if playerTurn else 2}: {board[move]}")
+        board.pop(move)
+        print()
+        print(board)
+        playerTurn = not playerTurn
--- Assignment06/tests.py
+++ Assignment06/tests.py
@@ -1,6 +1,7 @@
 import math
 
-from implementations import nimDP, nimMemoizing, nimRecursive, generateNim
+from implementations import nimDP, nimMemoizing, nimRecursive, generateNim, nimTraceback, printNim
+from implementations import concatRecursive, concatMemoizing, concatDP, concatTraceback, printConcat, generateConcat
 def testProblemsEqual(functions, generator, start, stop, problems, v=False):
     assert start % 2 == 0
     assert stop % 2 == 0
@@ -17,14 +18,15 @@ def testProblemsEqual(functions, generator, start, stop, problems, v=False):
         problemSet = [generator(n) for k in range(problems)]
 
         for problem in problemSet:
-            baseline = functions[0](problem)
+            baseline = functions[0](*problem)
             for j in range(len(functions)):
-                evaluation = functions[j](problem)
+                evaluation = functions[j](*problem)
                 if v:
                     print(f"    Function {j}: {evaluation} -> {'Pass' if evaluation == baseline else 'Fail'}")
 
                 if evaluation != baseline:
                     return False
+
         n *= 2
 
     return True
@@ -32,21 +34,35 @@ def testProblemsEqual(functions, generator, start, stop, problems, v=False):
 def testDP(function, problems, values, v=False):
     if v:
         print("Testing DP Algorithm:")
-    for i in range(len(problems)):
-        evaluation = function(problems[i])
+    for i, problem in enumerate(problems):
+        evaluation = function(*problem)
         if v:
             print(f"    Expected: {values[i]}, Result: {evaluation} -> {'Pass' if evaluation == values[i] else 'Fail'}")
         if evaluation != values[i]:
             return False
 
 
-
+def problem1():
+    KNOWN_PROBLEMS = [("catcatadogpersonctdogolargecatdog", ["cat", "dog", "person", "ct", "cata", "dogo", "large"]),
+                      ("ctctctctctctctctctctcta", ["cat", "dog", "person", "ct", "cata", "dogo", "large"]),
+                      ("abababbbbababbabaab", ["abaab", "babba", "aab", "bba"])]
+    KNOWN_SOLUTIONS = [True, False, False]
+    print("----------Equality Test----------")
+    testProblemsEqual([concatRecursive, concatMemoizing, concatDP], generateConcat, 8, 16, 10, True)
+    print("\n\n\n\n----------DP Test----------")
+    testDP(concatDP, KNOWN_PROBLEMS, KNOWN_SOLUTIONS, True)
+    print("\n\n\n\n----------TraceBack Test----------")
+    printConcat(concatTraceback(concatDP(*(KNOWN_PROBLEMS[0]), True), *KNOWN_PROBLEMS[0]), *KNOWN_PROBLEMS[0])
 
 def problem5():
     KNOWN_PROBLEMS = [[2, 12, 7, 14], [32, 8, 27, 54, 2, 8, 17, 10]]
     KNOWN_SOLUTIONS = [26, 104]
+    print("----------Equality Test----------")
     testProblemsEqual([nimRecursive, nimMemoizing, nimDP], generateNim, 2, 16, 1, True)
+    print("\n\n\n\n----------DP Test----------")
     testDP(nimDP, KNOWN_PROBLEMS, KNOWN_SOLUTIONS, True)
+    print("\n\n\n\n----------TraceBack Test----------")
+    printNim(nimTraceback(nimDP(KNOWN_PROBLEMS[1], True)), KNOWN_PROBLEMS[1])
 
 if __name__ == "__main__":
-    problem5()
+    problem1()
