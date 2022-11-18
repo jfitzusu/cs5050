d6afa2744554562148558c16d6bfe978e5d8c842
jfitzusu <A02261889@usu.edu>
2022-11-17 18:25:54 -0700

    Did Problems 2 and 4

--- Assignment06/implementations.py
+++ Assignment06/implementations.py
@@ -1,4 +1,6 @@
 import copy
+import math
+import queue
 import random
 
 import numpy as np
@@ -12,6 +14,7 @@ PROBLEM 1
 -------------------------------------------------
 -------------------------------------------------
 """
+
 
 def generateConcat(n, numberWords=10, minWord=2, maxWord=8):
     words = []
@@ -28,7 +31,6 @@ def generateConcat(n, numberWords=10, minWord=2, maxWord=8):
         randomWord = words[random.randint(0, numberWords - 1)]
         stringSize += len(randomWord)
         string.append(randomWord)
-
 
     isNotConcat = bool(random.randint(0, 1))
 
@@ -101,12 +103,12 @@ def concatDPHelper(S, dictionary, traceback):
     return results[-1]
 
 
-
 def concatTraceback(results, S, words):
     dictionary = {}
     for word in words:
         dictionary[word] = True
     return concatTracebackHelper(results, S, dictionary)
+
 
 def concatTracebackHelper(results, S, dictionary):
     i = len(results) - 1
@@ -119,7 +121,6 @@ def concatTracebackHelper(results, S, dictionary):
                 break
     words.reverse()
     return words
-
 
 
 def printConcat(words, S, wordList):
@@ -143,6 +144,89 @@ PROBLEM 2
 -------------------------------------------------
 -------------------------------------------------
 """
+
+def generateGrid(n, minJump=-4, maxJump=4):
+    moves = max(n // 10, 1)
+    moveList = []
+    for i in range(moves):
+        move = (random.randint(minJump, maxJump), random.randint(minJump, maxJump), random.randint(minJump, maxJump))
+        moveList.append(move)
+    start = (random.randint(0, n-1), random.randint(0, n-1), random.randint(0, n-1))
+    return (n, n, n), start, moveList
+# def generateGrid(n):
+#     return (20, 20, 20), (17, 15, 17), [(1, -3, -4), (0, 4, -4)]
+
+def gridMemoizing(grid, start, moves):
+    cache = np.array(np.zeros(grid))
+    gridMemoizingHelper(grid, *start, moves, cache)
+    return cache
+
+def gridMemoizingHelper(grid, x, y, z, moves, cache):
+    if x >= grid[0] or x < 0:
+        return
+    if y >= grid[1] or y < 0:
+        return
+    if z >= grid[2] or z < 0:
+        return
+
+    if cache[x][y][z]:
+        return
+
+    else:
+        cache[x][y][z] = 1
+
+    for move in moves:
+        gridMemoizingHelper(grid, x + move[0], y + move[1], z + move[2], moves, cache)
+
+def gridDP(grid, start, moves):
+    solutions = [[[(False, (0, 0, 0)) for i in range(grid[2])] for j in range(grid[1])] for k in range(grid[0])]
+    points = queue.Queue()
+    solutions[start[0]][start[1]][start[2]] = (True, (-1, -1, -1))
+    points.put(start)
+    while not points.empty():
+        currentPoint = points.get()
+        for move in moves:
+            newX = currentPoint[0] + move[0]
+            newY = currentPoint[1] + move[1]
+            newZ = currentPoint[2] + move[2]
+            if newX >= grid[0] or newX < 0:
+                continue
+            if newY >= grid[1] or newY < 0:
+                continue
+            if newZ >= grid[2] or newZ < 0:
+                continue
+            if not solutions[newX][newY][newZ][0]:
+                solutions[newX][newY][newZ] = (True, (currentPoint[0], currentPoint[1], currentPoint[2]))
+                points.put((newX, newY, newZ))
+    return solutions
+
+def gridTracebackSingle(solutions, point):
+    print(f"How to Get to Point {point}")
+    if not solutions[point[0]][point[1]][point[2]][0]:
+        print(    "Point Not Reachable")
+        return
+    path = []
+    path.append(point)
+    newX, newY, newZ = solutions[point[0]][point[1]][point[2]][1]
+    while newX != -1:
+        path.append((newX, newY, newZ))
+        newX, newY, newZ = solutions[newX][newY][newZ][1]
+    path.reverse()
+    previousPoint = "Start"
+    for i, point in enumerate(path):
+        print(f"    Move {i}: {previousPoint} -> {point}")
+        previousPoint = point
+
+
+
+def gridTracebackAll(solutions, startPoint):
+    print(f"Reachable Points from {startPoint}:")
+    for i in range(len(solutions)):
+        for j in range(len(solutions[i])):
+            for k in range(len(solutions[i][j])):
+                if solutions[i][j][k][0]:
+                    print(f"    {(i, j, k)}")
+
 """
 -------------------------------------------------
 -------------------------------------------------
@@ -152,6 +236,109 @@ PROBLEM 3
 -------------------------------------------------
 -------------------------------------------------
 """
+
+
+def generateKnapsack(n, items=2, itemMin=1, itemMax=10):
+    W = []
+    for i in range(items):
+        W.append(random.randint(itemMin, itemMax))
+    return (n, n, W)
+
+
+def knapsackRecursive(k1, k2, W):
+    return knapsackRecursiveHelper(k1, k2, len(W) - 1, W)
+
+
+def knapsackRecursiveHelper(k1, k2, i, W):
+    if k1 < 0 or k2 < 0:
+        return False
+
+    if k1 == 0 and k2 == 0:
+        return True
+
+    if i < 0:
+        return False
+
+    return max(knapsackRecursiveHelper(k1 - W[i], k2, i, W), knapsackRecursiveHelper(k1, k2 - W[i], i, W),
+               knapsackRecursiveHelper(k1, k2, i - 1, W))
+
+
+def knapsackMemoizing(k1, k2, W):
+    cache = np.array(np.zeros((k1 + 1, k2 + 1, len(W))), dtype=int)
+    cache.fill(-1)
+    return knapsackMemoizingHelper(k1, k2, len(W) - 1, W, cache)
+
+
+def knapsackMemoizingHelper(k1, k2, i, W, cache):
+    if k1 < 0 or k2 < 0:
+        return False
+
+    if k1 == 0 and k2 == 0:
+        return True
+
+    if i < 0:
+        return False
+
+    if cache[k1][k2][i] != -1:
+        return cache[k1][k2][i]
+
+    noTake = knapsackMemoizingHelper(k1, k2, i - 1, W, cache)
+    take1 = knapsackMemoizingHelper(k1 - W[i], k2, i, W, cache)
+    take2 = knapsackMemoizingHelper(k1, k2 - W[i], i, W, cache)
+
+    sol = max(noTake, take1, take2)
+    cache[k1][k2][i] = sol
+    return sol
+
+
+def knapsackDP(k1, k2, W, traceback=False):
+    resultsTable = np.array(np.zeros((k1 + 1, k2 + 1, len(W))), dtype=bool)
+
+    for i in range(len(W)):
+        resultsTable[0][0][i] = True
+    for i in range(len(resultsTable)):
+        for j in range(len(resultsTable[i])):
+            for k in range(len(W)):
+                if i == 0 and k == 0:
+                    break
+                noTake = resultsTable[i][j][k - 1]
+                take1 = False if W[k] > i else resultsTable[i - W[k]][j][k]
+                take2 = False if W[k] > j else resultsTable[i][j - W[k]][k]
+                resultsTable[i][j][k] = noTake or take1 or take2
+
+    if traceback:
+        return resultsTable
+
+    return resultsTable[-1][-1][-1]
+
+
+def knapsackTraceback(resultsTable, W):
+    i = len(resultsTable) - 1
+    j = len(resultsTable[i]) - 1
+    k = len(resultsTable[i][j]) - 1
+    items1 = []
+    items2 = []
+    while i >= 0 and j >= 0 and k >= 0:
+        if W[k] <= i and resultsTable[i - W[k]][j][k]:
+            items1.append(W[k])
+            i -= W[k]
+        if W[k] <= j and resultsTable[i][j - W[k]][k]:
+            items2.append(W[k])
+            j -= W[k]
+        else:
+            k -= 1
+    return items1, items2
+
+
+def printKnapsack(items1, items2, size1, size2, itemList):
+    print("Possible Items:")
+    print(itemList)
+    print(f"Items In Knapsack 1 ({size1})")
+    print(items1)
+    print(f"Items in Knapsack 2 ({size2})")
+    print(items2)
+
+
 """
 -------------------------------------------------
 -------------------------------------------------
@@ -161,6 +348,127 @@ PROBLEM 4
 -------------------------------------------------
 -------------------------------------------------
 """
+
+
+def generateBoard(n, minValue=0, maxValue=25):
+    return [[[random.randint(minValue, maxValue) for i in range(n)] for j in range(n)]]
+
+
+def boardRecursive(board):
+    if len(board) == 0:
+        return 0
+    assert len(board) == len(board[0])
+    return max([boardRecursiveHelper(board, 0, i, 0) for i in range(len(board))])
+
+
+def boardRecursiveHelper(board, x, y, total):
+    if y >= len(board[0]) or y < 0:
+        return -math.inf
+
+    total += board[x][y]
+    if x >= len(board) - 1:
+        return total
+
+    return max(boardRecursiveHelper(board, x + 1, y + 1, total), boardRecursiveHelper(board, x + 1, y - 1, total))
+
+
+def boardMemoizing(board):
+    if len(board) == 0:
+        return 0
+    assert len(board) == len(board[0])
+    cache = np.array(np.zeros((len(board), len(board[0]))))
+    cache.fill(-1)
+    return max([boardMemoizingHelper(board, 0, i, cache) for i in range(len(board))])
+
+
+def boardMemoizingHelper(board, x, y, cache):
+    if y >= len(board[0]) or y < 0:
+        return -math.inf
+
+    if x >= len(board) - 1:
+        return board[x][y]
+
+    if cache[x][y] != -1:
+        return cache[x][y]
+
+    maxUp = boardMemoizingHelper(board, x + 1, y + 1, cache)
+    maxDown = boardMemoizingHelper(board, x + 1, y - 1, cache)
+    return board[x][y] + max(maxUp, maxDown)
+
+
+def boardDP(board, traceBack=False):
+    solArray = np.array(np.zeros((len(board), len(board[0]))))
+    for i in range(len(solArray[0])):
+        solArray[-1][i] = board[-1][i]
+
+    for i in range(len(solArray) - 2, -1, -1):
+        for j in range(len(solArray[0])):
+            upValue = -math.inf if j >= len(solArray[0]) - 1 else solArray[i + 1][j + 1]
+            downValue = -math.inf if j <= 0 else solArray[i + 1][j - 1]
+            solArray[i][j] = board[i][j] + max(upValue, downValue)
+
+    if traceBack:
+        return solArray
+
+    return max(solArray[0])
+
+def boardTraceback(solArray):
+    currentX = 0
+    currentY = 0
+    maxScore = -math.inf
+    for i in range(len(solArray)):
+        if solArray[0][i] > maxScore:
+            currentY = i
+            maxScore = solArray[0][i]
+
+    moves = []
+    moves.append((currentX, currentY))
+    while currentX < len(solArray) - 1:
+
+        moveUp = -math.inf if currentY >= len(solArray[0]) - 1 else solArray[currentX + 1][currentY + 1]
+        moveDown = -math.inf if currentY <= 0 else solArray[currentX + 1][currentY - 1]
+        if moveUp > moveDown:
+            currentY += 1
+        else:
+            currentY -= 1
+        currentX += 1
+
+        moves.append((currentX, currentY))
+    return maxScore, moves
+
+def printBoard(maxScore, moves, board):
+    cellSize = math.floor(math.log(maxScore) / math.log(10)) + 4
+    print(f"Max Score: {maxScore}")
+    print("Starting Board:")
+    print("==============================")
+    niceBoard(board, cellSize)
+    trackBoard = copy.deepcopy(board)
+    currentScore = 0
+    for i in range(len(moves)):
+        currentScore += board[moves[i][0]][moves[i][1]]
+        print(f"Move {i}")
+        print("==============================")
+        copyBoard = copy.deepcopy(board)
+        copyBoard[moves[i][0]][moves[i][1]] = f'X({currentScore})'
+        trackBoard[moves[i][0]][moves[i][1]] = f'X({currentScore})'
+        niceBoard(copyBoard, cellSize)
+
+    print("Overall Path:")
+    print("==============================")
+    niceBoard(trackBoard, cellSize)
+
+
+def niceBoard(board, cellSize):
+    print("-" * ((len(board) * (cellSize + 3)) + 1))
+    for j in range(len(board[0])):
+        for i in range(len(board)):
+            if isinstance(board[i][j], int):
+                print(f"| {board[i][j]:>{cellSize}.0f} ", end='')
+            else:
+                print(f"| {board[i][j]:>{cellSize}s} ", end='')
+        print("|")
+        print("-" * ((len(board) * (cellSize + 3)) + 1))
+
 """
 -------------------------------------------------
 -------------------------------------------------
@@ -174,7 +482,7 @@ PROBLEM 5
 
 def generateNim(n):
     assert n % 2 == 0
-    return [random.randint(0, 100) for i in range(n)]
+    return [[random.randint(0, 100) for i in range(n)]]
 
 
 def nimRecursive(V):
--- Assignment06/tests.py
+++ Assignment06/tests.py
@@ -2,6 +2,10 @@ import math
 
 from implementations import nimDP, nimMemoizing, nimRecursive, generateNim, nimTraceback, printNim
 from implementations import concatRecursive, concatMemoizing, concatDP, concatTraceback, printConcat, generateConcat
+from implementations import knapsackRecursive, knapsackMemoizing, knapsackDP, knapsackTraceback, printKnapsack, generateKnapsack
+from implementations import generateBoard, boardRecursive, boardMemoizing, boardDP, boardTraceback, printBoard
+from implementations import generateGrid, gridMemoizing, gridDP, gridTracebackSingle, gridTracebackAll
+
 def testProblemsEqual(functions, generator, start, stop, problems, v=False):
     assert start % 2 == 0
     assert stop % 2 == 0
@@ -31,6 +35,38 @@ def testProblemsEqual(functions, generator, start, stop, problems, v=False):
 
     return True
 
+def testGridsEqual(start, stop, problems, v=False):
+    assert start % 2 == 0
+    assert stop % 2 == 0
+    assert stop > start
+
+    iterations = math.floor(math.log(stop / start) / math.log(2)) + 1
+
+
+    n = start
+    for i in range(iterations):
+        if v:
+            print(f"Testing Problem Size {n}")
+
+        problemSet = [generateGrid(n) for k in range(problems)]
+
+        for p, problem in enumerate(problemSet):
+            memoizing = gridMemoizing(*problem)
+            dynamic = gridDP(*problem)
+            match = True
+            for i in range(n):
+                for j in range(n):
+                    for k in range(n):
+                        match = match and (memoizing[i][j][k] == dynamic[i][j][k][0])
+            print(f"    Problem {p}-> {'Pass' if match else 'Fail'}")
+
+            if not match:
+                return False
+
+        n *= 2
+
+    return True
+
 def testDP(function, problems, values, v=False):
     if v:
         print("Testing DP Algorithm:")
@@ -40,6 +76,19 @@ def testDP(function, problems, values, v=False):
             print(f"    Expected: {values[i]}, Result: {evaluation} -> {'Pass' if evaluation == values[i] else 'Fail'}")
         if evaluation != values[i]:
             return False
+
+def testGridDP(problems, solutions):
+    print("Testing DP Algorithm:")
+    for p, problem in enumerate(problems):
+        result = gridDP(*problem)
+        success = True
+        for i in range(len(solutions[p])):
+            for j in range(len(solutions[p][i])):
+                for k in range(len(solutions[p][i][j])):
+                    success = success and (solutions[p][i][j][k] == result[i][j][k][0])
+
+        print(f"    Expected: {solutions[p]}\n    Result: {result}\n    -> {'Pass' if success else 'Fail'}")
+
 
 
 def problem1():
@@ -54,15 +103,50 @@ def problem1():
     print("\n\n\n\n----------TraceBack Test----------")
     printConcat(concatTraceback(concatDP(*(KNOWN_PROBLEMS[0]), True), *KNOWN_PROBLEMS[0]), *KNOWN_PROBLEMS[0])
 
+def problem2():
+    KNOWN_PROBLEMS = [[(3, 3, 3), (0, 0, 0), [(1, 0, 0), (0, 1, 0), (0, 0, 1)]]]
+    KNOWN_SOLUTIONS = [[[[True, True, True], [True, True, True], [True, True, True]], [[True, True, True], [True, True, True], [True, True, True]], [[True, True, True], [True, True, True], [True, True, True]]]]
+    print("----------Equality Test----------")
+    testGridsEqual(16, 32, 1, True)
+    print("\n\n\n\n----------DP Test----------")
+    testGridDP(KNOWN_PROBLEMS, KNOWN_SOLUTIONS)
+    print("\n\n\n\n----------TraceBack Test----------")
+    print(f"Grid Size: {KNOWN_PROBLEMS[0][0]}")
+    print(f"Starting Point: {KNOWN_PROBLEMS[0][1]}")
+    print(f"Moves: {KNOWN_PROBLEMS[0][2]}")
+    gridTracebackSingle(gridDP(*KNOWN_PROBLEMS[0]), (2, 2, 2))
+    gridTracebackAll(gridDP(*KNOWN_PROBLEMS[0]), (0, 0, 0))
+
+
+def problem3():
+    KNOWN_PROBLEMS = [[9, 11, [3, 2]], [12, 32, [5, 8]], [1, 23, [1]]]
+    KNOWN_SOLUTIONS = [True, False, True]
+    print("----------Equality Test----------")
+    testProblemsEqual([knapsackRecursive, knapsackMemoizing, knapsackDP], generateKnapsack, 4, 8, 10, True)
+    print("\n\n\n\n----------DP Test----------")
+    testDP(knapsackDP, KNOWN_PROBLEMS, KNOWN_SOLUTIONS, True)
+    print("\n\n\n\n----------TraceBack Test----------")
+    printKnapsack(*knapsackTraceback(knapsackDP(*KNOWN_PROBLEMS[0], True), KNOWN_PROBLEMS[0][2]), *KNOWN_PROBLEMS[0])
+
+def problem4():
+    KNOWN_PROBLEMS = [[[[0, 7, 8], [12, 0, 0], [1, 1, 1]]], [[[12, 12, 12], [1, 7, 9], [52, 0, 0]]], [[[8, 5, 6], [6, 5, 8], [12, 7, 3]]]]
+    KNOWN_SOLUTIONS = [20, 71, 25]
+    print("----------Equality Test----------")
+    testProblemsEqual([boardRecursive, boardMemoizing, boardDP], generateBoard, 4, 16, 2, True)
+    print("\n\n\n\n----------DP Test----------")
+    testDP(boardDP, KNOWN_PROBLEMS, KNOWN_SOLUTIONS, True)
+    print("\n\n\n\n----------TraceBack Test----------")
+    printBoard(*boardTraceback(boardDP(*KNOWN_PROBLEMS[1], True)), *KNOWN_PROBLEMS[1])
+
 def problem5():
-    KNOWN_PROBLEMS = [[2, 12, 7, 14], [32, 8, 27, 54, 2, 8, 17, 10]]
+    KNOWN_PROBLEMS = [[[2, 12, 7, 14]], [[32, 8, 27, 54, 2, 8, 17, 10]]]
     KNOWN_SOLUTIONS = [26, 104]
     print("----------Equality Test----------")
     testProblemsEqual([nimRecursive, nimMemoizing, nimDP], generateNim, 2, 16, 1, True)
     print("\n\n\n\n----------DP Test----------")
     testDP(nimDP, KNOWN_PROBLEMS, KNOWN_SOLUTIONS, True)
     print("\n\n\n\n----------TraceBack Test----------")
-    printNim(nimTraceback(nimDP(KNOWN_PROBLEMS[1], True)), KNOWN_PROBLEMS[1])
+    printNim(nimTraceback(nimDP(*KNOWN_PROBLEMS[1], True)), *KNOWN_PROBLEMS[1])
 
 if __name__ == "__main__":
-    problem1()
+    problem2()
